46551ab8c1eba663c29319de3dc93dde0976cd7e
illicchpv <illich58@mail.ru>
2024-01-24 18:04:46 +0300

    урок15 - Библиотеки React для работы с формами+

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import {FormLib} from './FormLib';
 function App() {
   return (
     <div className="App-header">
-      <Form />
-      <div style={{height: '20px'}}></div>
       <FormLib />
+      <div style={{height: '20px'}}></div>
+      <Form />
     </div>
   );
 }
--- src/FormLib.jsx
+++ src/FormLib.jsx
@@ -1,8 +1,58 @@
+import {useState} from 'react';
 import _ from './FormLib.module.css';
+import {useForm} from 'react-hook-form';
 
 export const FormLib = () => {
+  const {
+    register,
+    handleSubmit, // занимается проверками. если ок - вызывает переданную ей ф-ю
+    formState: {errors}, // объявляет переменную состояния формы. доступна внутри FormLib
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log('onSubmit data: ', data);
+  }
+  console.log('errors: ', errors);
 
   return (
-    <div className={_.FormLib}>FormLib</div>
-  )
-}
+    <form className={_.form} onSubmit={handleSubmit(onSubmit)}>
+      <p>библиотека react-hook-form:</p>
+      <div className={_.wrap}>
+        <label className={_.label} htmlFor="email">email</label>
+        <input className={_.input} id="email" type="text" autoComplete="off"
+          {...register('email', {
+            required: // 'введите email'
+            { // или
+              value: true,
+              message: 'укажите email адрес',
+            },
+            pattern: {
+              value: /^.+@.+\..+$/,
+              message: 'неверный формат email',
+            }
+          })}
+          aria-invalid={!!errors.email}
+        />
+        {errors.email && <p className={_.error}>{errors.email.message}</p>}
+      </div>
+
+      <div className={_.wrap}>
+        <label className={_.label} htmlFor="password">password</label>
+        <input className={_.input} id="password" type="text" autoComplete="off"
+          {...register('password')}
+        />
+        <p className={_.error}>сообщение об ошибке</p>
+      </div>
+
+      <div className={_.wrapCheckbox}>
+        <input className={_.checkbox} id="save" type="checkbox"
+          {...register('save')}
+        />
+        <label className={_.labelCheckbox} htmlFor="save">запомнить пароль</label>
+      </div>
+
+      <button className={_.submit} type='submit'>войти</button>
+
+    </form>
+  );
+};
